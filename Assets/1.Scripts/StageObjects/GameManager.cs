7a9bbb70ba1f5f04c8601aa25c03038c0b3a6e80
jungyounghan <superbrush@naver.com>
2025-02-19 21:49:03 +0900

    [fix] Start 함수에서 GameObject 비교 시 에디터 터지는 버그를 발견

--- Assets/1.Scripts/StageObjects/GameManager.cs
+++ Assets/1.Scripts/StageObjects/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,22 +7,49 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private State _state;
     [SerializeField]
-    private GameObject _obstacleObjects;
-    [SerializeField]
     private Controller _playerController;
+    [SerializeField]
+    private GameObject[] _props;
 
-    private void Awake()
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        if (_obstacleObjects != null)
+        for(int i = 0; i < _props.Length; i++)
         {
-            _obstacleObjects.SetActive(true);
+            if(_props[i] != null)
+            {
+                _props[i].SetActive(false);
+            }
         }
+    }
+#endif
+
+    IEnumerator Start()
+    {
+        for (int i = 0; i < _props.Length; i++)
+        {
+            Debug.Log("실행중");
+            Debug.Log(_props[i] != null);
+            bool isTrue = (_props[i] != null);
+            Debug.Log("실행중22");
+            if (isTrue)
+            {
+                _props[i].SetActive(true);
+            }
+          
+            //{
+            //    Debug.Log("2");
+            //    _props[i].SetActive(true);
+            //    Debug.Log("3");
+            //}
+        }
+        Debug.Log(_props.Length);
         if (_playerController != null)
         {
             _playerController.Initialize(SetStamina, SetLife);
-            _playerController.gameObject.SetActive(true);
             _playerController.Revive();
         }
+        yield return null;
     }
 
     public void SetLife(uint current, uint max)
